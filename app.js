720379f908044cdcb7bb74d8234c88a4ea7b9a1f
akash12patel12 <akash12patel12@gmail.com>
2022-10-31 20:01:03 +0530

    addproduct post route

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
-
-app.use((req,res,next)=>{
-    console.log("In middleware 1");
-    next();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/add-product',(req,res)=>{
+    // console.log("In middleware 1");
+    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 })
-app.use((req,res,next)=>{
-    console.log("In middleware 2");
+
+app.post('/product',(req,res)=>{
+     console.log(req.body);
+     res.redirect('/');
+});
+
+app.use('/',(req,res)=>{
+    // console.log("In middleware 2");
     res.send('<h1>Hello from express</h1>')
 })
 
